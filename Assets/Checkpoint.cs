b31756ba1c92fecfd54f8500901161ab055669d7
Jack K <jack.karuneswaran@gmail.com>
2019-12-05 01:24:10 +0100

    Level 6 WIP + Checkpoints

---
+++ Assets/Checkpoint.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Checkpoint : MonoBehaviour
+{
+    [SerializeField] private UnityEngine.SpriteRenderer _treeLeaves;
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("CHANGE");
+        _treeLeaves.color = Color.white;
+    }
+}
