85bd1917f98677970a0e81f13f6a0f03006a574d
Jack K <jack.karuneswaran@gmail.com>
2020-03-16 23:04:48 +0100

    Water order layer

---
+++ Assets/Scripts/PlaneSortingLayer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaneSortingLayer : MonoBehaviour
+{
+    [SerializeField] private int _sortingOrder = -1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.GetComponent<MeshRenderer>().sortingLayerName = "Terrain";
+        transform.GetComponent<MeshRenderer>().sortingOrder = _sortingOrder;
+    }
+
+}
