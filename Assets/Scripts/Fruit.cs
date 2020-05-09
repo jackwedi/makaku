c3b99bad6b5ea1c8a632beb55432399cc8f10d28
Jack K <jack.karuneswaran@gmail.com>
2020-05-10 01:31:42 +0200

    particles fruit collected

--- Assets/Scripts/Fruit.cs
+++ Assets/Scripts/Fruit.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 
 public class Fruit : MonoBehaviour
 {
+    [SerializeField] private GameObject _particles = null;
+
     private void OnCollisionEnter2D(Collision2D other) {
         if (other.gameObject.CompareTag("Player")) {
             this.gameObject.SetActive(false);
+            Instantiate(_particles, transform.position, Quaternion.identity);
             Messenger.Broadcast(GameEvent.FRUIT_COLLECTED.ToString());
         }
     }
